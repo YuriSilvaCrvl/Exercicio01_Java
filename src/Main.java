0c322a7a4b70c83c365c0a13e833836171cc220e
YuriSilvaCrvl <yurisilvacrvl@gmail.com>
2025-02-18 23:24:58 -0300

    Initial commit

---
+++ src/Main.java
@@ -0,0 +1,10 @@
+public class Main {
+    public static void main(String[] args) {
+        int a = 1;
+        int b = 2;
+        int c = 3;
+        int resultado = (a * b * c);
+
+        System.out.println("o resultado da multiplicação de " +a+ "," +b+ "," +c+ " é:" + resultado);
+    }
+}
