800b134ca1e0815a7d00996f814f7e5e9589da53
YuriSilvaCrvl <yurisilvacrvl@gmail.com>
2025-02-20 23:40:08 -0300

    ex2

--- src/Main.java
+++ src/Main.java
@@ -1,9 +1,19 @@
+import java.util.Scanner;
+
 public class Main {
     public static void main(String[] args) {
-        float a = 1.5f;
-        float c = 3.2f;
-        float resultado = (c%a);
+        Scanner a = new Scanner(System.in);
 
-        System.out.println("o resultado é:" + resultado);
+        //Ler dois números e realizar a soma
+
+        System.out.print("Digite o primeiro número:");
+        int n1 = a.nextInt();
+        System.out.print("Digite o segundo número:");
+        int n2 = a.nextInt();
+        System.out.print("Digite o terceiro número:");
+        int n3 = a.nextInt();
+
+        int soma = (n1 + n2 + n3);
+        System.out.println("O resultado foi: " + soma);
     }
 }
