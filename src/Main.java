baa2c74fd7487a9748715dc4f8c186f109e73bc4
YuriSilvaCrvl <yurisilvacrvl@gmail.com>
2025-02-21 00:05:09 -0300

    exercício nome

--- src/Main.java
+++ src/Main.java
@@ -4,16 +4,12 @@ public class Main {
     public static void main(String[] args) {
         Scanner a = new Scanner(System.in);
 
-        //Ler dois números e realizar a soma
+        //Ler nome e sobrenome e juntar os dois
+        System.out.println("Digite o seu nome:");
+        String nome = a.nextLine();
+        System.out.println("Agora, o seu sobrenome:");
+        String sobre = a.nextLine();
 
-        System.out.print("Digite o primeiro número:");
-        int n1 = a.nextInt();
-        System.out.print("Digite o segundo número:");
-        int n2 = a.nextInt();
-        System.out.print("Digite o terceiro número:");
-        int n3 = a.nextInt();
-
-        int soma = (n1 + n2 + n3);
-        System.out.println("O resultado foi: " + soma);
+        System.out.println("Bom, o seu nome é: "+ nome +" " + sobre);
     }
 }
